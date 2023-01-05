79e8fbca2f8e25981ba416a5b015468d8c05f8e6
Egor Badulin <nobodykenny@gmail.com>
2023-01-05 21:15:24 +0300

    feat: add icons

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -1,5 +1,50 @@
-import camera from "../assets/sprite/icon-camera.svg";
+import arrowDouble from "../assets/sprite/icon-arrow-double.svg";
+import iconArrowLeft from "../assets/sprite/icon-arrow-left.svg";
+import iconArrowRight from "../assets/sprite/icon-arrow-right.svg";
+import iconArrowTop from "../assets/sprite/icon-arrow-top.svg";
+import iconBox from "../assets/sprite/icon-box.svg";
+import iconCheck from "../assets/sprite/icon-check.svg";
+import iconClose from "../assets/sprite/icon-close.svg";
+import iconControl from "../assets/sprite/icon-control.svg";
+import iconEurope from "../assets/sprite/icon-europe.svg";
+import iconFile from "../assets/sprite/icon-file.svg";
+import iconHouse from "../assets/sprite/icon-house.svg";
+import iconMarker from "../assets/sprite/icon-marker.svg";
+import iconMenu from "../assets/sprite/icon-menu.svg";
+import iconPerson from "../assets/sprite/icon-person.svg";
+import iconPhrase from "../assets/sprite/icon-phrase.svg";
+import iconPlane from "../assets/sprite/icon-plane.svg";
+import iconSearch from "../assets/sprite/icon-search.svg";
+import iconShield from "../assets/sprite/icon-shield.svg";
+import iconShip from "../assets/sprite/icon-ship.svg";
+import iconStorage from "../assets/sprite/icon-storage.svg";
+import iconTransport from "../assets/sprite/icon-transport.svg";
+import iconTruck from "../assets/sprite/icon-truck.svg";
+import iconVk from "../assets/sprite/icon-vk.svg";
+
 
 export default {
-    camera
+    arrowDouble,
+    iconArrowLeft,
+    iconArrowRight,
+    iconArrowTop,
+    iconBox,
+    iconCheck,
+    iconClose,
+    iconControl,
+    iconEurope,
+    iconFile,
+    iconHouse,
+    iconMarker,
+    iconMenu,
+    iconPerson,
+    iconPhrase,
+    iconPlane,
+    iconSearch,
+    iconShield,
+    iconShip,
+    iconStorage,
+    iconTransport,
+    iconTruck,
+    iconVk
 };
